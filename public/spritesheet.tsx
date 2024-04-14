ab161ccd76b2ae6705fd33bf0519b09303666c22
airtonDomiciano <airton_mio@hotmail.com>
2024-04-14 15:37:20 -0300

    implementacao da casa

---
+++ public/spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="32" tileheight="32" tilecount="285" columns="19">
+ <image source="spritesheet.png" width="624" height="496"/>
+</tileset>
